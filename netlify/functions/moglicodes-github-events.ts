93a95b8dbbb47c6ccede6c25c05286663c96d09a
MogliCodes <moglicodes@gmail.com>
2022-10-15 23:40:23 +0200

    Add example fetch call

--- netlify/functions/moglicodes-github-events.ts
+++ netlify/functions/moglicodes-github-events.ts
@@ -1,10 +1,14 @@
 const { schedule } = require('@netlify/functions');
+import { $fetch } from 'ohmyfetch'
 
-console.log('SERVERLESS')
+console.log('SERVERLESS OUTSIDE')
 
 const handler = async function(event, context) {
+    const { users } = await $fetch('https://api.github.com/users/MogliCodes/events')
+    console.log('users', users)
+
     console.log("Received event:", event);
-    console.log('SERVERLESS')
+    console.log('SERVERLESS INSIDE')
     return {
         statusCode: 200,
     };
